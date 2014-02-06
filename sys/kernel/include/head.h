59c89dba63ca3c8e716eb1f9b662c94529d74b58
moyilong <moyilong@foxmail.com>
2014-02-06 19:59:12 +0800

    更新版本 Signed-off-by: moyilong <moyilong@foxmail.com>

--- sys/kernel/include/head.h
+++ sys/kernel/include/head.h
@@ -1,3 +1,3 @@
-k_version=Ivy-R1235-D
-support_pkgver=ELA;ELAv2;AGP;ELAv3;ELAv3.1;ELAv3.2
-pkg_version=Socket-ELA-3.2
+k_version=Longen-E2050AB
+support_pkgver=ELA;ELAv2;AGP;ELAv3;ELAv3.1;ELAv3.2;ELAv3.2A
+pkg_version=Socket-ELA-3.2A
