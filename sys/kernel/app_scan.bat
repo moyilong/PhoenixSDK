686d7687b636eae088925ddcfc3663c1d0fb2f6c
moyilong <moyilong@foxmail.com>
2014-02-25 21:27:32 +0800

    APP-Scan添加对更多后缀应用模组的扫描
    Signed-off-by: moyilong <moyilong@foxmail.com>

--- sys/kernel/app_scan.bat
+++ sys/kernel/app_scan.bat
@@ -1,3 +1,3 @@
 cd %appdir%
-for /d %%n in (*.ppk) do if exist %appdir%\%%n\data\META-INFO call %kernel%\add_feature.bat %%n & echo FInd Modules:%%n
+for /d %%n in (*.EPX;*.K-LIB;*.lib;*.app;kmod-*;*.ppk) do if exist %appdir%\%%n\data\META-INFO call %kernel%\add_feature.bat %%n & echo FInd Modules:%%n
 cd %initdir%
