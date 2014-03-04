95764959e18665fa71fa81a6a19a6ca2772e3ac8
moyilong <moyilong@foxmail.com>
2014-03-04 09:45:05 +0800

    更新版本号
    Signed-off-by: moyilong <moyilong@foxmail.com>

--- sys/kernel/include/head.h
+++ sys/kernel/include/head.h
@@ -1,6 +1,6 @@
-k_version=Longen-E2050AI
+k_version=OpteronE-T2440GX
 support_pkgver=ELA;ELAv2;ELAv3;ELAv3.1;ELAv3.2;ELAv3.2A;ELX;EXT
-pkg_version=Socket-ELX-3.9
+pkg_version=Socket-ELX-4.0T
 
-ppk_label=ELA;ELAv2;ELAv3;ELAv3.1;ELAv3.2;ELAv3.2A
+ppk_label=ELA;ELAv2;ELAv3;ELAv3.1;ELAv3.2;ELAv3.2A;4.0T
 ext_label=ELX;EXT
