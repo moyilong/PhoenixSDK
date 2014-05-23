35d4eda6adfebd65ba226763c8dcde7e2fb7d9ce
moyilong <moyilong@foxmail.com>
2014-05-23 19:17:43 +0800

    更新版本号
    Signed-off-by: moyilong <moyilong@foxmail.com>

--- sys/kernel/include/head.h
+++ sys/kernel/include/head.h
@@ -1,6 +1,6 @@
-k_version=OpteronE-T2605AD-GDR
+k_version=OpteronE-T2606AD-GDR
 pkg_version=Socket-ELX-4.2
 ppk_label=ELA;ELAv2;ELAv3;ELAv3.1;ELAv3.2;ELAv3.2A;ELAv4
 ext_label=ELX;EXT
 srv_workmod=INIT;WORK;DISABLE
-api=10
+api=10.1
