96ba8917469317a0ea0495a5b319b434d4d966c8
moyilong <moyilong@foxmail.com>
2014-06-04 23:40:04 +0800

    更新WIMMAKE
    Signed-off-by: moyilong <moyilong@foxmail.com>

--- sys/modules/AFT.ppk/api/make_mkp_wim.bat
+++ sys/modules/AFT.ppk/api/make_mkp_wim.bat
@@ -1,3 +1,9 @@
+@echo off
+echo WIM Backup Maker
+set t=%date:/=-%
+set t=%t:~0,-3% %time::=_%
+set t=%t:.=__%
+echo Get Time ID=%t%
 mkdir %proc%\wim_maker
 mkdir %proc%\wim_maker\boot
 mkdir %proc%\wim_maker\recovery
@@ -15,12 +21,18 @@ echo 正在生成WIM文件
 move %proc%\AFT %proc%\wim_maker\update_final
 cd /d %proc%
 imagex /capture wim_maker temp.wim "AFT Backup Image %build%" /COMPRESS %wim_compress%
-imagex /export temp.wim 1 %out%\image_build.wim "AFT Backup Image %build%"
-echo =================Android Firmware Tool %build%===========>>%out%\hash_table.sha1
-echo Build ID %build% at %date% %time%>>%out%\hash_table.sha1
-echo Build Script=%bs%
-sha1sum -r wim_maker >>%out%\hash_table.sha1
-echo =================Android Firmware Tool %build%===========>>%out%\hash_table.sha1
+imagex /export temp.wim 1 %out%\image_build.wim "AFT Backup Image %build%" "WIM Image Backup %build% at %date% %time%"
+echo =================Android Firmware Tool %build%===========>%temp%\hash_table.%t%.sha1
+echo Build ID %build% at %date% %time%>>%temp%\hash_table.%t%.sha1
+echo Build Script=%bs%>>%temp%\hash_table.%t%.sha1
+echo Time= %date% %time%>>%temp%\hash_table.%t%.sha1
+sha1sum -r wim_maker >>%temp%\hash_table.%t%.sha1
+echo =================Android Firmware Tool %build%===========>>%temp%\hash_table.%t%.sha1
+zip a %out%\hash_table.hstab %temp%\hash_table.%t%.sha1 -t7z
+echo [%t%]Add Compile Project >>%out%\hstab_chlog.log
+sha1sum %out%\hash_table.hstab >>%out%\hstab_chlog.log
+
+del %temp%\hash_table.%t%.sha1
 del temp.wim
 rmdir /q /s wim_maker
 cd /d %out%
