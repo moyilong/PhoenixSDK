d9e384ad63ed8ca95ab5600fe37dd306b002d27c
moyilong <moyilong@foxmail.com>
2014-06-05 14:07:17 +0800

    修复deodex工具中的一个错误
    Signed-off-by: moyilong <moyilong@foxmail.com>

--- sys/modules/kmod-aft/ANDFIRv2-rc2/firmware/path/deodex.bat
+++ sys/modules/kmod-aft/ANDFIRv2-rc2/firmware/path/deodex.bat
@@ -1,4 +1,14 @@
 @echo off
+if "%1"=="" goto arg_error
+
 call %appdir%\AFT.ppk\api\deodex.bat %initdir%\update\system\app %1
 call %appdir%\AFT.ppk\api\deodex.bat %initdir%\update\system\framework %1
 if exist %initdir%\update\system\priv-app call %appdir%\AFT.ppk\api\deodex.bat %initdir%\update\system\priv-app %1
+goto end
+
+:arg_error
+echo Unknow Arg
+echo Useage:
+echo deodex API_LEVEL
+goto end
+:end
