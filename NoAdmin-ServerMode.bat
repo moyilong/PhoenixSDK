382c7a1298da81af705aeb66d374e068705033e3
moyilong <moyilong@foxmail.com>
2014-06-16 21:22:53 +0800

    内核版本更新
    Signed-off-by: moyilong <moyilong@foxmail.com>

--- NoAdmin-ServerMode.bat
+++ NoAdmin-ServerMode.bat
@@ -9,6 +9,7 @@ set clean_www=false
 set server_initdir=%initdir%
 set www=%server_initdir%\www
 call %kernel%\include.bat %sdkdir%\include\server_configure.h
+set serverdir=%server_initdir%
 for %%f in (%server_initdir%;%server_initdir%\task;%server_initdir%\task\tm;%www%;%www%\sha_tbl) do if not exist %%f mkdir %%f
 for %%f in (%feature%) do if exist %appdir%\%%f\www call %kernel%\init_www.bat %%f
 for %%f in (%feature%) do if exist %appdir%\%%f\server_init.bat start /MIN %appdir%\%%f\server_init.bat __SERVER_INIT__
@@ -25,10 +26,11 @@ echo 触发时间:%dt%  %tm%
 if exist %server_dir%\task\%dt%\%tm%.bat call %server_dir%\task\%dt%\%tm%.bat >>%sys_log%
 if exist %server_dir%\task\tm\%tm%.bat call %server_dir%\task\tm\%tm%.bat >>%sys_log%
 
-if not exist %server_dir%\task\%dt%\%tm%.bat 没有日期+时间触发项目
+if not exist %server_dir%\task\%dt%\%tm%.bat echo 没有日期+时间触发项目
 if not exist %server_dir%\task\tm\%tm%.bat echo 没有时间触发项目
 
-
+::RPC Control
+::Task Regist
 
 
 sleep 60
--- sys/kernel/include/head.h
+++ sys/kernel/include/head.h
@@ -1,6 +1,6 @@
-k_version_n=1
+k_version_n=2
 pkg_version=Socket-ELX-5
 ppk_label=ELA;ELAv2;ELAv3;ELAv3.1;ELAv3.2;ELAv3.2A;ELAv4;ELAv4.1;ELAv5
 ext_label=ELX;EXT
 srv_workmod=INIT;WORK;DISABLE
-api=10.2
+api=11
