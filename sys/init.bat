2848753ce9a5a07ecfc82a0b94c9d324ffb05f09
moyilong <moyilong@foxmail.com>
2014-11-18 19:11:44 +0800

    Signed-off-by: moyilong <moyilong@foxmail.com>

--- sys/init.bat
+++ sys/init.bat
@@ -1,4 +1,5 @@
 @echo off
 cd /d  %~dp0
 cd ..
+set INITER=%0
 cmd.exe /k sys\kernel\bootable\bootloader.bat sys ni ni
--- sys/kernel/bootable/efi.bat
+++ sys/kernel/bootable/efi.bat
@@ -28,7 +28,11 @@ echo Alloc dllSemphella / libSemphella
 echo Semphella ArgProcess Support
 vecho \v\n
 cd /d %kernel% 
-for /r %%f in (*.APX) do  copy %%f %APX_COLLECT_DIR%\
+for /r %%f in (*.APX,*.apd) do  copy %%f %APX_COLLECT_DIR%\
+
+
+
+
 echo %date% %time% EFI System Init Over! >>%sys_log%
 
 
