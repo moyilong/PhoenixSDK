d6dc606c6b394f7aa3cb36d0af3ae5618a531306
moyilong <moyilong@foxmail.com>
2014-12-01 20:34:33 +0800

    Big Update

--- sys/kernel/logo.bat
+++ sys/kernel/logo.bat
@@ -7,4 +7,6 @@ echo          /      /     /   /   /\    /   /          %name%
 echo         /----  /     /   /   /  \  /   /----       %load_string%
 echo        /____  /___  /___/   /    \/   /____        %k_string% %string_later%
 echo.                                                   %kernel_mode_string%
+echo           tbHO0rHks8nO0tb3xsGx2ta9tcTKsbryo6zE48rHt/G7ubvh1Nq69c7S
 echo               %version% Build %build_id% at %k_version%
+call %kernel%\line.bat
